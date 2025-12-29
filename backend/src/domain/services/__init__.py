c774e61b4c554549de5050870c8d566f4ede5760
Leo Guanco <leo.guanco@gmail.com>
2025-12-28 21:53:42 -0300

    chore: Remove file.chore: Remove file.

--- backend/src/domain/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Domain Services package.
-
-Note: Services have been refactored to Rich Domain Entities.
-This package is kept for future domain services that coordinate
-multiple entities (not stateless calculators).
-"""
-
-__all__ = []
